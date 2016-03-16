990a92a5c16a72f3809e8addc62f4d7cb6071661
Piyumi A <imuyip@gmail.com>
2016-03-16 21:09:33 +0000

    first commit

---
+++ main.js
@@ -0,0 +1,19 @@
+  var svg = d3.select("body").append("svg").attr("height","1000").attr("width","1000");
+  var mindate = new Date(2016,0,13),
+            maxdate = new Date(2016,0,14);
+
+        var xScale = d3.time.scale()
+          .domain([mindate, maxdate])
+    .range([30,880]);
+
+     var xAxis = d3.svg.axis()
+      .scale(xScale)
+      .tickSize(5)
+      .ticks(10)
+      .orient("bottom");
+
+  d3.select("svg").append("g")
+  .attr("class", "xaxis")
+  .attr("transform", "translate(0," + (300) + ")")
+
+  .call(xAxis);
---
+++ scripts/main.js
@@ -0,0 +1,21 @@
+var svg = d3.select("#timeline").append("svg").attr("height","1000").attr("width","1000");
+var mindate = new Date(2016,0,13)
+  , maxdate = new Date(2016,0,14);
+
+var xScale = d3.time
+  .scale()
+  .domain([mindate, maxdate])    // values between for month of january
+  .range([30,880]);
+
+var xAxis = d3.svg
+  .axis()
+  .scale(xScale)
+  .tickSize(5)
+  .ticks(10)
+  .orient("bottom");
+
+d3.select("svg")
+  .append("g")
+  .attr("class", "xaxis")
+  .attr("transform", "translate(0," + (300) + ")")
+  .call(xAxis);
