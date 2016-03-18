886c2b65930c71796951396fb4998feea0f7e9f9
Piyumi A <imuyip@gmail.com>
2016-03-18 17:24:43 +0000

    orientation now controllable by a single variable

--- scripts/main.js
+++ scripts/main.js
@@ -2,6 +2,7 @@ var svg = d3.select("#timeline").append("svg").attr("height","1000").attr("width
 var mindate = new Date(2016,0,13)
   , maxdate = new Date(2016,0,14);
 
+orient="left"; //control whether horizontal or vertical (bottom or left)
 var scale = d3.time
   .scale()
   .domain([mindate, maxdate])
@@ -12,10 +13,20 @@ var axis = d3.svg
   .scale(scale)
   .tickSize(5)
   .ticks(10)
-  .orient("left"); //control whether horizontal or vertical
+  .orient(orient);
 
 d3.select("svg")
   .append("g")
   .attr("class", "axis")
-  .attr("transform", "translate(100)")
+  .attr("transform", function() {
+    if (orient=="left") {
+      return "translate(50)";
+    }
+    else if (orient=="bottom") {
+      return "translate(0,450)";
+    }
+    else {
+    console.log("not a valid orientation");
+    };
+  })
   .call(axis);
