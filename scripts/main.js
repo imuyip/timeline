4e106c8823f5d5626cade1b3c6579300943bd43a
Piyumi A <imuyip@gmail.com>
2016-03-18 15:49:20 +0000

    changed axis from horizontal to vertical

--- scripts/main.js
+++ scripts/main.js
@@ -2,20 +2,20 @@ var svg = d3.select("#timeline").append("svg").attr("height","1000").attr("width
 var mindate = new Date(2016,0,13)
   , maxdate = new Date(2016,0,14);
 
-var xScale = d3.time
+var yScale = d3.time
   .scale()
   .domain([mindate, maxdate])
-  .range([30,880]);
+  .range([880,30]);
 
-var xAxis = d3.svg
+var yAxis = d3.svg
   .axis()
-  .scale(xScale)
+  .scale(yScale)
   .tickSize(5)
   .ticks(10)
-  .orient("bottom");
+  .orient("left");
 
 d3.select("svg")
   .append("g")
   .attr("class", "axis")
-  .attr("transform", "translate(0," + (300) + ")")
-  .call(xAxis);
+  .attr("transform", "translate(100)")
+  .call(yAxis);
