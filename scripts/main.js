f948822fe63cf35a0d081ad4466bb3e55ec4f4d7
Piyumi A <imuyip@gmail.com>
2016-03-21 11:39:45 +0000

    altered ticksize and styles

--- scripts/main.js
+++ scripts/main.js
@@ -9,7 +9,7 @@ var width = parseFloat(svg.style("width"))-30
 var mindate = new Date(2016,0,13)
   , maxdate = new Date(2016,0,14);
 
-var orient="left"; //control whether horizontal or vertical (bottom or left)
+var orient="bottom"; //control whether horizontal or vertical (bottom or left)
 var scale = d3.time
   .scale()
   .domain([mindate, maxdate])
@@ -18,14 +18,17 @@ var scale = d3.time
 var axis = d3.svg
   .axis()
   .scale(scale)
-  .tickSize(5)
+  .tickSize(-((orient==="bottom")*((width)*(3/5)) + (orient==="left")*(height*(6/5)))) //will fix this later
   .ticks(10)
+  .tickPadding(10)
   .orient(orient);
 
 d3.select("svg")
   .append("g")
   .attr("class", "axis")
   .attr("transform", function() {
-    return orient=="bottom" ? "translate(0," + height + ")" : "translate(50)";
+    return orient=="bottom" ? "translate(0," + height + ")" : "translate(60)";
   })
-  .call(axis);
+  .call(axis)
+
+d3.select(".domain").on("mouseover", function() { console.log("domain mouseover")});
