9eb9c53f00a01a1d57013456534e35b776c474bf
Piyumi A <imuyip@gmail.com>
2016-03-21 22:38:40 +0000

    created mock sections, limited zoom, added text

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,9 @@
+//TODO: fix tick changes
+//TODO: make it respond to window resizing
+//TODO: neater way to create different sections
+//TODO: update orientation
+//TODO: add ticks on other side of timeline axis
+
 var svg = d3.select("#timeline")
   .append("svg")
   .attr("height","100%")
@@ -6,10 +12,25 @@ var svg = d3.select("#timeline")
 var width = parseFloat(svg.style("width"))-30
   , height = parseFloat(svg.style("height"))-30;
 
-var mindate = new Date(2016,0,13)
-  , maxdate = new Date(2016,0,14);
+var mindate = new Date(2014,11,16)
+  , maxdate = new Date(2016,3,21);
 
-var orient="left"; //control whether horizontal or vertical (bottom or left)
+var userOrients = ["horizontal","vertical"]
+var orients=["bottom","left"];
+var orient=orients[1]; 
+svg.append("text")
+        .text("Make me vertical")
+        .attr("x", (width-150))
+        .attr("y", 20)
+        .attr("text-anchor", "middle")
+        .style("font-size", "14")
+        .style("font-family", "sans-serif")
+        .style("fill","grey")
+        .on("click", function(){
+          d3.select(this).text("Make me horizontal");
+          orient="left";
+        })
+
 var scale = d3.time
   .scale()
   .domain([mindate, maxdate])
@@ -19,6 +40,22 @@ var axis = d3.svg
   .axis()
   .scale(scale)
   .tickSize(-((orient==="bottom")*(height-30) + (orient==="left")*(width-40)))
+  .ticks(5)
+  .tickPadding(6)
+  .orient(orient);
+
+var axis2 = d3.svg
+  .axis()
+  .scale(scale)
+  .tickSize(-((orient==="bottom")*(height-30) + (orient==="left")*(width-40))/3)
+  .ticks(10)
+  .tickPadding(10)
+  .orient(orient);
+
+var axis3 = d3.svg
+  .axis()
+  .scale(scale)
+  .tickSize(-((orient==="bottom")*(height-30) + (orient==="left")*(width-40))*2/3)
   .ticks(10)
   .tickPadding(10)
   .orient(orient);
@@ -31,7 +68,34 @@ d3.select("svg")
   })
   .call(axis);
 
+  d3.select("svg")
+    .append("g")
+    .attr("class", "axis2")
+    .attr("transform", function() {
+      return orient=="bottom" ? "translate(0," + height + ")" : "translate(60)";
+    })
+    .call(axis2);
+
+  d3.select("svg")
+    .append("g")
+    .attr("class", "axis3")
+    .attr("transform", function() {
+      return orient=="bottom" ? "translate(0," + height + ")" : "translate(60)";
+    })
+    .call(axis3);
+
+svg.append("text")
+        .text("D3 timeline")
+        .attr("class","title")
+        .attr("x", (width-35))
+        .attr("y", 20)
+        .attr("text-anchor", "middle")
+        .style("font-size", "14")
+        .style("font-family", "sans-serif")
+        .style("fill","grey");
+
 var zoom = d3.behavior.zoom()
+  .scaleExtent([0.3,150])
   .on("zoom", draw);
 
 svg.call(zoom);
